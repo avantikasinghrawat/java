0ff30b1af1803f6c77c8fae9c97d2a0b9ff3b68a
avantikasinghrawat <avantikasinghrawat02@gmail.com>
2023-05-18 21:44:34 +0530

    table

--- test.java
+++ test.java
@@ -1,1 +1,23 @@
-efthytsdsfsf
+import java.util.*;
+public class Main
+{
+    public static void main(String[] args) 
+    {
+        int a[];
+        int i,size;
+        Scanner ob = new  Scanner (System.in);
+        System.out.print("Enter the size of array:");
+        size=ob.nextInt();
+        a= new int [size];
+        System.out.print("Enter the vlaue:");
+        for(i=0;i<=size;i++)
+        {
+            a[i]=ob.nextInt();
+        }
+        for(i=0;i<=size;i++)
+        {
+            System.out.println("You enetered:"+a[i]);
+        }
+        
+    }
+}
