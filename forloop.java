e42b7ee7075cbb9091b6ffa0f8e98f8025910048
avantikasinghrawat <avantikasinghrawat02@gmail.com>
2023-05-19 09:58:29 +0530

    test3.java

---
+++ forloop.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class Avni
+{
+    public  static void  main(String arr[])
+    {
+        String st[]={"asha","harshita","mohan"};
+        int i[]={2,5,3,4,7};
+        for(int k:i)
+        {
+            System .out.println(k+" ");
+        }
+        for (String z:st)
+        {
+            System.out.println(z+" ");
+        }
+    
+    }
+}
